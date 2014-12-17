597597229cb67be010702cc63086b7584f7a58e7
bencomp <ben@companjen.name>
2014-12-17 12:52:07 +0100

    Use new source file in build-test

--- build-test.sh
+++ build-test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 [ -e eadtest ] && rm -r eadtest
+echo `date` > validation.log
 mkdir eadtest
 cd eadtest
-java -Xmx1g -jar ~/Applications/saxon9he.jar -explain -traceout:trace.txt -s:/Users/ben/Documents/Projecten/EHRI/ushmm/1000docs2.xml -xsl:../solr2ead.xsl
+java -Xmx1g -jar ~/Applications/saxon9he.jar -explain -traceout:trace.txt -s:/Users/ben/Documents/Projecten/EHRI/ushmm/1000docs5.xml -xsl:../solr2ead.xsl
 cd ..
 for FILE in `ls eadtest/ead/`; do
     xmllint --noout --schema ead.xsd eadtest/ead/$FILE/* 2>> validation.log
